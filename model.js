25d7de9f152fe618ec33c79de8ab9e6139c4d1ea
meisterk <franz@kohnlehome.de>
2019-11-26 15:23:17 +0100

    Add methods

--- model.js
+++ model.js
@@ -1,5 +1,17 @@
 export class Model {
     constructor() {
+        this._zahl = 0;
+    }
 
+    hochzaehlen() {
+        this._zahl++;
+    }
+
+    reset() {
+        this._zahl = 0;
+    }
+
+    getZahl() {
+        return this._zahl;
     }
 }
