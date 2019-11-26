2e1af3661e9a339629da7923e0aec66b65a78235
meisterk <franz@kohnlehome.de>
2019-11-26 15:18:01 +0100

    Add View

--- presenter.js
+++ presenter.js
@@ -1,7 +1,9 @@
 import { Model } from "./model.js";
+import { View } from "./view.js";
 
 export class Presenter {
     constructor() {
         this.model = new Model();
+        this.view = new View(this);
     }
 }
---
+++ view.js
@@ -0,0 +1,5 @@
+export class View {
+    constructor(presenter) {
+        this.presenter = presenter;
+    }
+}
